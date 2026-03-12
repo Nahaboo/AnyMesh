2cac443e3dd1e5e6961f3cf12ac5697d0877f15f
Nahaboo <60737461+Nahaboo@users.noreply.github.com>
2026-03-13 00:51:41 +0100

    fix: add low_cpu_mem_usage=False to BiRefNet from_pretrained

--- tools/trellis2_runpod/patch_birefnet.py
+++ tools/trellis2_runpod/patch_birefnet.py
@@ -1,9 +1,9 @@
 path = '/workspace/TRELLIS.2/trellis2/pipelines/rembg/BiRefNet.py'
 src = open(path).read()
 old = 'AutoModelForImageSegmentation.from_pretrained(\n            model_name, trust_remote_code=True\n        )'
-new = 'AutoModelForImageSegmentation.from_pretrained(\n            model_name, trust_remote_code=True, device_map=None\n        )'
+new = 'AutoModelForImageSegmentation.from_pretrained(\n            model_name, trust_remote_code=True, device_map=None, low_cpu_mem_usage=False\n        )'
 if old in src:
     open(path, 'w').write(src.replace(old, new))
-    print('Patched BiRefNet.py: device_map=None added')
+    print('Patched BiRefNet.py: device_map=None + low_cpu_mem_usage=False added')
 else:
     print('WARNING: pattern not found in BiRefNet.py, patch skipped')
